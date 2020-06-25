14fb79065c5bb41eda9fc3935a1af5713e774a1c
Sebeom Lee <moses97@gmail.com>
2020-06-25 18:07:57 +0900

    Add keras codes for conv2d learning

--- keras_env/data_gen.py
+++ keras_env/data_gen.py
@@ -1,8 +1,14 @@
 import cv2
 from glob import glob
-from os import path
+import numpy as np
 
-for filepath in glob('../data/jpg/*.jpg'):
-    image = cv2.imread(filepath)
-    image = cv2.resize(image, (224, 224))
-    cv2.imwrite(f'./DataSet/{path.basename(filepath)}', image)
+before = []
+after = []
+image = None
+for file in glob('./DataSet/*.jpg'):
+    before.append(image)
+    image = cv2.imread(file) / 255
+    after.append(image)
+
+np.save('./before.npy', np.array(before[1:]), allow_pickle=True)
+np.save('./after.npy', np.array(after[:-1]), allow_pickle=True)
